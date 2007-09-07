f35554f6dd8f5f820c7c3cc75c5e3ae44b5b73ee
tom.lazar <tom.lazar@ba3d73cc-f836-0410-9d4a-05498620a423>
2007-09-07 23:52:04 +0000

    updated for 0.2rc1
    git-svn-id: https://py-transmission.googlecode.com/svn/trunk@11 ba3d73cc-f836-0410-9d4a-05498620a423

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 dependencies = ["BitTorrent-bencode>=5.0.8"]
 setup(
     name = "TransmissionClient",
-    version = "0.1",
+    version = "0.2rc1",
     packages = find_packages(),
 
     # metadata for upload to PyPI
@@ -21,7 +21,7 @@ setup(
     test_suite = "test.testTransmissionClient",
     long_description = """Transmission has recently introduced a RPC architecture in which it launches an independent daemon listening on a local socket and exposes a rich API for monitoring and controlling Transmission. This makes it much easier and 'cleaner' to implement clients in other languages, which is what this package aims to do.
 """,
-    classifiers=["Development Status :: 3 - Alpha",
+    classifiers=["Development Status :: 4 - Beta",
         "Intended Audience :: Developers",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
